2a77b303f36390548353733942c842537effec63
patricktelnoni <patricktelnoni@users.noreply.github.com>
2022-04-07 07:13:50 +0000

    Awal Try dan Catch

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args)
+  {
+    try{
+      int a = 3/2;
+      int[] daftarAngka = {1,2};
+      System.out.println(a);
+      System.out.println(daftarAngka[100]);
+    }
+    catch(ArithmeticException e){
+      System.out.println("Operasi pembagian dengan 0 tidak valid");
+    }
+    catch(ArrayIndexOutOfBoundsException e){
+      System.out.println("Indeks array hanya bisa berisi 2");
+    }
+    finally{
+      System.out.println("Exception telah berhasil ditangani");
+    }
+    System.out.println("Program selesai");
   }
 }
